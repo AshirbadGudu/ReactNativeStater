8ab3bdfc38dc1c4c767e4b867dcc30bdef8593af
AshirbadGudu <ashirbadapanigrahi@gmail.com>
2020-11-15 16:48:24 +0530

    Search Feature Created

--- src/screens/Search.js
+++ src/screens/Search.js
@@ -1,15 +1,79 @@
-import React from 'react';
-import {View} from 'react-native';
+import {
+  Left,
+  ListItem,
+  List,
+  Thumbnail,
+  Body,
+  Text,
+  Right,
+  Button,
+  View,
+} from 'native-base';
+import React, {useEffect, useState} from 'react';
+import {ScrollView} from 'react-native-gesture-handler';
 import Topbar from '../components/Topbar';
-
+import SearchInput, {createFilter} from 'react-native-search-filter';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import {Dimensions} from 'react-native';
 const Search = (props) => {
+  const [posts, setPosts] = useState([]);
+  const [searchTex, setSearchTex] = useState('');
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/albums/1/photos')
+      .then((value) => {
+        value.json().then((data) => setPosts(data));
+      })
+      .catch((e) => console.log(e));
+  }, []);
+  const filteredPosts = posts.filter(createFilter(searchTex, ['title']));
+
   return (
-    <View>
+    <>
       <Topbar
         title={'Search'}
         left={{onPress: () => props.navigation.toggleDrawer()}}
       />
-    </View>
+      <View
+        style={{
+          flexDirection: 'row',
+          alignItems: 'center',
+          borderBottomWidth: 1,
+          borderBottomColor: '#ddd',
+        }}>
+        <Ionicons.Button
+          backgroundColor="transparent"
+          name="search"
+          color="#333"
+          size={24}
+        />
+        <View style={{width: '100%'}}>
+          <SearchInput
+            onChangeText={(txt) => setSearchTex(txt)}
+            placeholder="Search Post Here.."
+          />
+        </View>
+      </View>
+
+      <ScrollView>
+        <List>
+          {filteredPosts.map((item, key) => (
+            <ListItem thumbnail key={key}>
+              <Left>
+                <Thumbnail square source={{uri: item.thumbnailUrl}} />
+              </Left>
+              <Body>
+                <Text>{item.title}</Text>
+              </Body>
+              <Right>
+                <Button transparent>
+                  <Text>View</Text>
+                </Button>
+              </Right>
+            </ListItem>
+          ))}
+        </List>
+      </ScrollView>
+    </>
   );
 };
 
